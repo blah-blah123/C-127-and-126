af64f9dc80f87cf958f6d8427bb20ad95e98d7a9
blah-blah123 <104911572+blah-blah123@users.noreply.github.com>
2022-07-24 11:23:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+song= "";
+song2="";
+
+function setup(){
+    canvas= createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+video.hide();
+}
+
+function draw(){
+    image(video , 0 , 0 , 600 , 500);
+}
+
+function preload(){
+    song= loadSound("music.mp3");
+}
+
+function play(){
+song.play();
+song2.stop();
+}
+
+function play2(){
+    song.stop();
+    song2.play();
+}
